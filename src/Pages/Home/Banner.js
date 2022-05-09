e40bc80d48fb833b704f6b0c88667a73bae6b836
Sakib Rahman <seu.sakib@gmail.com>
2022-05-10 00:55:56 +0600

    add Banner

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,33 @@
+import React from "react";
+import bannerPic from "../../assets/images/chair.png";
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero min-h-screen ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={bannerPic}
+            className="max-w-sm rounded-lg shadow-2xl w"
+            alt="banner pic"
+          />
+          <div className="mr-20">
+            <h1 className="text-5xl font-bold">
+              Your New Smile Starts <br />
+              Here
+            </h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-secondary text-white">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
