f9b7a0f0789f31f349a67ea45c1b1c35210deddc
Sakib Rahman <seu.sakib@gmail.com>
2022-05-13 12:05:34 +0600

    fix .env

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,9 +1,43 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
-    <div>
-      <h1>This is login</h1>
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="mx-7">
+          <h2 class="text-lg text-center mb-9 mt-6">Login</h2>
+          <form>
+            <p>Email</p>
+            <input
+              type="text"
+              placeholder="Email "
+              class="input input-bordered  w-full max-w-lg mb-3"
+            />
+            <p>Password</p>
+            <input
+              type="password"
+              placeholder="Password"
+              class="input input-bordered  w-full max-w-lg"
+            />
+            <Link to="/reset" className="text-xs  btn-link">
+              Forget Password?
+            </Link>
+            <button type="submit" class="btn btn-block mt-5">
+              Login
+            </button>
+          </form>
+          <div class="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            class="btn btn-outline uppercase mb-7 mt-6 flex w-ful mx-auto"
+          >
+            Continue with google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,16 +6,17 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.React_App_apiKey,
-  authDomain: process.env.React_App_authDomain,
-  projectId: process.env.React_App_projectId,
-  storageBucket: process.env.React_App_storageBucket,
-  messagingSenderId: process.env.React_App_messagingSenderId,
-  appId: process.env.React_App_appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
+
 export default auth;
