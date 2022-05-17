9fcfc9b69f6c3713009f3ea8853ae9c0c882f88d
Sakib Rahman <seu.sakib@gmail.com>
2022-05-17 19:54:27 +0600

    handle img

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,22 +1,71 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 
 const AddDoctor = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
+    reset,
   } = useForm();
 
+  const { data: services, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/services").then((res) => res.json())
+  );
+
+  const imgStorageKey = "9e8e2acbf56d42fb4f9548e7cd46ea49";
+
   const onSubmit = async (data) => {
-    console.log("data", data);
+    console.log(data);
+    const image = data.photo[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imgStorageKey}`;
+
+    fetch(url, { method: "POST", body: formData })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.success) {
+          const img = result.data.url;
+          const doctor = {
+            name: data.name,
+            email: data.email,
+            specialty: data.specialty,
+            img: img,
+          };
+          // send to your database
+          fetch("http://localhost:5000/doctor", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+            body: JSON.stringify(doctor),
+          })
+            .then((res) => res.json())
+            .then((inserted) => {
+              inserted.insertedId
+                ? toast.success("Doctor added successfully")
+                : toast.error("Failed to add the doctor");
+              reset();
+              console.log("doctor", inserted);
+            });
+        }
+      });
   };
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div>
       <h2>Add new doctor</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* Name */}
+
         <label className="label">
           <span className="label-text">Name</span>
         </label>
@@ -71,25 +120,43 @@ const AddDoctor = () => {
           )}
         </label>
 
-        {/* Password */}
+        {/* Specialty */}
         <label className="label">
           <span className="label-text mt-3">Specialty</span>
         </label>
+
+        <select
+          {...register("specialty")}
+          className="select w-full select-bordered max-w-lg block"
+        >
+          {services.map((service) => (
+            <option key={service._id} value={service.value}>
+              {service.name}
+            </option>
+          ))}
+        </select>
+
+        {/* Photo */}
+
+        <label className="label">
+          <span className="label-text">Photo</span>
+        </label>
         <input
-          type="text"
-          placeholder="Specialty"
-          className="input input-bordered w-full max-w-lg"
-          {...register("specialty", {
+          type="file"
+          placeholder="Photo"
+          className="input input-bordered w-full max-w-lg "
+          {...register("photo", {
             required: {
               value: true,
-              message: "Specialization is Required",
+              message: "photo is Required",
             },
           })}
         />
+
         <label className="label">
-          {errors.specialty?.type === "required" && (
+          {errors.photo?.type === "required" && (
             <span className="label-text-alt text-red-600">
-              {errors.specialty.message}
+              {errors.photo.message}
             </span>
           )}
         </label>
